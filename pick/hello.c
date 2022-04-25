a7b10970d1b3318b6a4f3efbbceb27e491c94798
OctavPin <101955238+OctaPin@users.noreply.github.com>
2022-04-25 15:53:44 -0300

    Add files via upload

---
+++ pick/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    char nombre[50];
+
+    printf("Ingrese su nombre: ");
+    scanf("%s",nombre);
+    printf("hola %s!\n",nombre);
+    
+    return 0;    
+}
